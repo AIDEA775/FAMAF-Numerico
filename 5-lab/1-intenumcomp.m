3c5909c9cfcbd8420d10e8ad6f204246ab3929e0
Alejandro Ismael Silva <alex.sai95@gmail.com>
2017-06-02 17:51:16 -0300

    Day 9.0 (true?)

--- 5-lab/1-intenumcomp.m
+++ 5-lab/1-intenumcomp.m
@@ -1,3 +1,12 @@
 function intenumcomp(@fun, a, b, N, regla)
-  
+  switch (regla)
+    case 'trapecio'
+      #
+      return (fun(a) + fun(b)) / 2 * (b - a);
+    case 'pm'
+      #
+    case 'simpson'
+      #
+    otherwise
+      disp("Fail.")
 endfunction
